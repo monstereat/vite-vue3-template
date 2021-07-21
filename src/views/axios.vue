d24b9276f9e23a0afc177818559ebdd85cb35469
陈达 <532160108@qq.com>
2021-07-21 10:29:03 +0800

    init route vuex element-plus axios  stylus

---
+++ src/views/axios.vue
@@ -0,0 +1,19 @@
+<template></template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import axios from '../utils/axios'
+
+export default defineComponent({
+  setup() {
+    console.log('axios', axios)
+    axios
+      .get('/users/monstereat')
+      .then((res) => {
+        console.log('res: ', res)
+      })
+      .catch((err) => {
+        console.log('err: ', err)
+      })
+  },
+})
+</script>
