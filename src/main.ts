026530d8c95fbf415f7912254af1b10c7114f641
陈达 <532160108@qq.com>
2022-02-14 22:03:04 +0800

    feat：引入element-plus

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,8 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 
-createApp(App).use(router).use(store).mount('#app')
+
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+
+createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
