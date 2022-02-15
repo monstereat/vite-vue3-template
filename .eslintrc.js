8c8274376c5701baad3367eaf687717ef506b6cb
陈达 <532160108@qq.com>
2022-02-15 11:46:44 +0800

    feat：配置husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    'vue/setup-compiler-macros': true
   },
   extends: [
     'plugin:vue/essential',
@@ -19,5 +20,32 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    'vue/multi-word-component-names': 0,
+    'vue/no-multiple-template-root': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    "no-param-reassign": [
+      "error",
+      {
+        "props": true,
+        "ignorePropertyModificationsFor": [
+          "e", // for e.returnvalue
+          "ctx", // for Koa routing
+          "req", // for Express requests
+          "request", // for Express requests
+          "res", // for Express responses
+          "response", // for Express responses
+          "state" // for vuex state
+        ]
+      }
+    ],
+    /*'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: ['state', 'config']
+      }
+    ]*/
+    "no-console":"off"
   },
 };
--- src/views/axios.vue
+++ src/views/axios.vue
@@ -1,9 +1,12 @@
-<template></template>
+<template>
+  <div></div>
+</template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 import axios from '../utils/axios'
 
 export default defineComponent({
+  name: 'axios',
   setup() {
     axios
       .get('/users/XPoet')
@@ -13,6 +16,6 @@ export default defineComponent({
       .catch((err) => {
         console.log('err: ', err)
       })
-  },
+  }
 })
 </script>
