89f28ff60b878115237071ce9a55e82d11096889
陈达 <532160108@qq.com>
2022-02-15 15:28:48 +0800

    feat(deps): 添加单元测试，集成@types/jest，添加 eslint-plugin-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,44 +8,42 @@ module.exports = {
   extends: [
     'plugin:vue/essential',
     'airbnb-base',
-    'plugin:prettier/recommended' // 添加 prettier 插件
+    'plugin:prettier/recommended', // 添加 prettier 插件
+    'plugin:jest/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-  ],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
     'vue/multi-word-component-names': 0,
     'vue/no-multiple-template-root': 'off',
     'import/no-unresolved': 'off',
     'import/extensions': 'off',
-    "no-param-reassign": [
-      "error",
+    'no-param-reassign': [
+      'error',
       {
-        "props": true,
-        "ignorePropertyModificationsFor": [
-          "e", // for e.returnvalue
-          "ctx", // for Koa routing
-          "req", // for Express requests
-          "request", // for Express requests
-          "res", // for Express responses
-          "response", // for Express responses
-          "state" // for vuex state
+        props: true,
+        ignorePropertyModificationsFor: [
+          'e', // for e.returnvalue
+          'ctx', // for Koa routing
+          'req', // for Express requests
+          'request', // for Express requests
+          'res', // for Express responses
+          'response', // for Express responses
+          'state' // for vuex state
         ]
       }
     ],
-    /*'no-param-reassign': [
+    /* 'no-param-reassign': [
       'error',
       {
         props: true,
         ignorePropertyModificationsFor: ['state', 'config']
       }
-    ]*/
-    "no-console":"off"
-  },
-};
+    ] */
+    'no-console': 'off'
+  }
+}
